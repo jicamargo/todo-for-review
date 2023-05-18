9babd6e5517b3e0d536134240c4c7cdf5cf5d920
jic <ji_camargo@yahoo.com>
2023-05-17 23:21:43 -0500

    copy all files deleted accidentally

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import './style.css';
+import iconRefresh from './images/icon-refresh.svg';
+import iconReturn from './images/icon-return.svg';
+import getTasks from './gettasks.js';
+import showTaskList from './showtasklist.js';
+import addNewTask from './addnewtask.js';
+import clearCompletedTasks from './clearcompletedtasks.js';
+
+const iconRefreshElem = document.getElementById('iconRefresh');
+const iconReturnElem = document.getElementById('iconReturn');
+iconRefreshElem.src = iconRefresh;
+iconReturnElem.src = iconReturn;
+
+const arrTasks = getTasks();
+showTaskList(arrTasks, 'list');
+
+const newTaskEnterBtn = document.getElementById('newTaskEnterBtn');
+newTaskEnterBtn.addEventListener('click', () => { addNewTask(arrTasks); });
+
+const newTaskInput = document.getElementById('new-task');
+newTaskInput.addEventListener('change', () => { addNewTask(arrTasks); });
+
+const clearCompletedBtn = document.getElementById('btnClearCompleted');
+clearCompletedBtn.addEventListener('click', (event) => clearCompletedTasks(event, arrTasks));
