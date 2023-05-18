314fdd78bd6a16a59f17a6c47f9ddad0e11ff2d3
jic <ji_camargo@yahoo.com>
2023-05-18 16:40:14 -0500

    add updatetask.test.js - checktaskchange.test.js - clearcompleted.test.js

---
+++ src/checktaskchange.test.js
@@ -0,0 +1,76 @@
+import checkTask from './checktaskchange';
+import saveTasks from './savetasks';
+import blinkEffect from './blinkeffect';
+
+jest.mock('./savetasks', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+jest.mock('./blinkeffect', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+describe('checkTask', () => {
+  let taskCheckboxMock;
+  let arrTasksMock;
+  let taskMock;
+  let taskDescriptionMock;
+  let parentElementMock;
+
+  beforeEach(() => {
+    // Arrange
+    taskCheckboxMock = {
+      checked: false,
+      nextElementSibling: null,
+    };
+
+    arrTasksMock = [];
+
+    taskMock = {
+      completed: false,
+    };
+
+    taskDescriptionMock = {
+      classList: {
+        toggle: jest.fn(),
+      },
+      parentElement: {
+        classList: {
+          toggle: jest.fn(),
+        },
+      },
+    };
+
+    parentElementMock = {
+      classList: {
+        toggle: jest.fn(),
+      },
+    };
+
+    global.document = {
+      createElement: jest.fn(() => taskDescriptionMock),
+    };
+
+    saveTasks.mockClear();
+    blinkEffect.mockClear();
+  });
+
+  test('checks task, updates UI and saves tasks', () => {
+    // Arrange
+    taskCheckboxMock.checked = true;
+    taskCheckboxMock.nextElementSibling = taskDescriptionMock;
+    taskDescriptionMock.parentElement = parentElementMock;
+
+    // Act
+    checkTask(taskCheckboxMock, arrTasksMock, taskMock);
+
+    // Assert
+    expect(taskMock.completed).toBe(true);
+    expect(taskDescriptionMock.classList.toggle).toHaveBeenCalledWith('task-completed');
+    expect(parentElementMock.classList.toggle).toHaveBeenCalledWith('shine-task-completed');
+    expect(blinkEffect).toHaveBeenCalledWith(taskDescriptionMock.parentElement);
+    expect(saveTasks).toHaveBeenCalledWith(arrTasksMock);
+  });
+});
---
+++ src/clearcompletedtasks.test.js
@@ -0,0 +1,75 @@
+import clearCompletedTasks from './clearcompletedtasks';
+import saveTasks from './savetasks';
+import showTaskList from './showtasklist';
+import blinkEffect from './blinkeffect';
+
+jest.mock('./savetasks', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+jest.mock('./showtasklist', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+jest.mock('./blinkeffect', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+describe('clearCompletedTasks', () => {
+  let eventMock;
+  let arrTasksMock;
+
+  beforeEach(() => {
+    // Arrange
+    eventMock = {
+      target: {},
+    };
+
+    arrTasksMock = [
+      { completed: true },
+      { completed: false },
+      { completed: true },
+      { completed: false },
+    ];
+
+    saveTasks.mockClear();
+    showTaskList.mockClear();
+    blinkEffect.mockClear();
+  });
+
+  test('clears completed tasks, updates UI, and saves tasks', () => {
+    // Arrange
+    eventMock.target = {};
+
+    // Act
+    clearCompletedTasks(eventMock, arrTasksMock);
+
+    // Assert
+    expect(arrTasksMock.length).toBe(2);
+    expect(arrTasksMock[0].completed).toBe(false);
+    expect(arrTasksMock[1].completed).toBe(false);
+    expect(saveTasks).toHaveBeenCalledWith(arrTasksMock);
+    expect(showTaskList).toHaveBeenCalledWith(arrTasksMock, 'list');
+    expect(blinkEffect).toHaveBeenCalledWith(eventMock.target);
+  });
+
+  test('does nothing when there are no completed tasks', () => {
+    // Arrange
+    arrTasksMock = [
+      { completed: false },
+      { completed: false },
+    ];
+
+    // Act
+    clearCompletedTasks(eventMock, arrTasksMock);
+
+    // Assert
+    expect(arrTasksMock.length).toBe(2);
+    expect(saveTasks).not.toHaveBeenCalled();
+    expect(showTaskList).not.toHaveBeenCalled();
+    expect(blinkEffect).toHaveBeenCalled();
+  });
+});
